5a8c0459b3cd723abe9b7ec10283ce54bc40bc1d
Oleg Soloviev <oleg.soloviev@gmail.com>
2023-06-17 15:41:09 +0200

    test LS-unwrap on a circular aperture

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,19 +4,40 @@ using Test
 @testset "PhaseUtils.jl" begin
     # Write your tests here.
 
+    @testset "utils" begin
+        ttt = ap2mask([0.5,0])
+        @test all([ttt[1] == 1, ismissing(ttt[2])])
+        @test all(mask2ap(ttt) .== [1,0])
+    end
+
+    @testset "differentiations" begin
     aaa = reshape(1:9, (3, 3))
 
     ## Phase differentiations
     @test all(
         PhaseUtils._calculate_gradient(aaa, PhaseUtils.FiniteDifferencesCyclic()) .==  
         [[-6 3 3; -6 3 3; -6 3 3], [-2 -2 -2; 1 1 1; 1 1 1] ])
+    end
+
+    @testset "PhaseUnwrapping" begin
+        s1,s2,m = 140,100,25
+        wedge = [0.5i for i in 1:s1, j in 1:s2]
+        wedge_ap  = [ m<i<s1-m && 25<j<s2-m for i in 1:s1, j in 1:s2]
+        wedge .*= wedge_ap
+        sol = unwrap_LS(phwrap(wedge), wedge_ap, restore_piston = true)
+        @test all(phwrap(sol .* wedge_ap) .≈ phwrap(wedge .* wedge_ap))
+
+        # Example with a circular aperture
+        ap = zeros(s1,s2)
+        y = range(-1.1 *s1/s2,1.1 *s1/s2,s1)
+        x = range(-1.1,1.1,s2)
+        ap[[x.^2 + y.^2 .<= 1 for y in y, x in x]] .= 1
+        phaseGT = [-x^3 + 3x.^2 + y.^2 - 10y for y in y, x in x]
+        phase = phwrap(phaseGT) .*ap
+        sol = unwrap_LS(phase, ap, restore_piston = true)
+        @test all(phwrap(phaseGT .- sol) .* ap  .+ 1 .≈ 1.)  # the difference can be 2pi*k, so we wrap it
+        # relatively to 1., the error should be negligible.
+    end
 end
 
-@testset "PhaseUnwrapping" begin
-    s1,s2,m = 100,140,25
-    wedge = [0.5i for i in 1:s1, j in 1:s2]
-    wedge_ap  = [ m<i<s1-m && 25<j<s2-m for i in 1:s1, j in 1:s2]
-    wedge .*= wedge_ap
-    sol = unwrap_LS(phwrap(wedge), wedge_ap, restore_piston = true)
-    @test all(phwrap(sol .* wedge_ap) .≈ phwrap(wedge .* wedge_ap))
-end
+
