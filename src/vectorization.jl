68d718252e9b7b864966591df45bf50b53c99d71
Oleg Soloviev <oleg.soloviev@gmail.com>
2025-05-02 10:13:00 +0200

    Refactor toArray and _toArray functions for improved clarity and maintainability

--- src/vectorization.jl
+++ src/vectorization.jl
@@ -19,6 +19,29 @@ function toArray(
 
     retsize = Tuple(ex[2]) .- startoffset .+ shift .+ 1
 
+    # ret = zeros(promote_type(eltype(vals), typeof(defaultel)), retsize)
+    # if defaultel != 0
+    #     ret .= defaultel
+    # end
+    # offset = CartesianIndex(to_indices(ret, 1 .- startoffset .+ shift))
+    # ret[ind .+ offset] .= vals
+
+    # return ret
+
+    return _toArray(ind, vals, retsize, startoffset; shift=shift, defaultel=defaultel)
+
+end
+
+toArray(ind::Array{Tuple}, args...; kwargs...) =
+    toArray(CartesianIndex.(ind), args...; kwargs...)
+
+toArray(ind::Array{T}, vals, retsize::Tuple; kwargs...) where {T<:CartesianIndex} =
+    _toArray(ind, vals, retsize, Tuple(oneunit(T)); kwargs...)
+
+
+function _toArray(
+    ind::Array{T}, vals, retsize::Tuple, startoffset::Tuple; shift=0, defaultel=0
+) where {T<:CartesianIndex}
     ret = zeros(promote_type(eltype(vals), typeof(defaultel)), retsize)
     if defaultel != 0
         ret .= defaultel
@@ -27,8 +50,4 @@ function toArray(
     ret[ind .+ offset] .= vals
 
     return ret
-
-
 end
-
-toArray(ind::Array{Tuple}, vals; kwargs...) = toArray(CartesianIndex.(ind), vals; kwargs...)
